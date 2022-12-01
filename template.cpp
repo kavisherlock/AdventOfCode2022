1c4a13235420e85b0bbd2eb008e029f606db13c9
kavisherlock <kavishrmunjal@gmail.com>
2022-12-01 10:18:28 -0600

    initial commit

---
+++ template.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+vector<string> split(string s, string delimiter) {
+  vector<string> results;
+  size_t pos = 0;
+  string token;
+  while ((pos = s.find(delimiter)) != string::npos) {
+      token = s.substr(0, pos);
+      results.push_back(token);
+      s.erase(0, pos + delimiter.length());
+  }
+  results.push_back(s);
+  return results;
+}
+
+int main () {
+  ifstream myfile;
+  myfile.open ("../input");
+  vector<string> lines;
+  if (myfile.is_open()) {
+    string lineString;
+    while(getline(myfile, lineString)) {
+      lines.push_back(lineString);
+    }
+  }
+  myfile.close();
+}
