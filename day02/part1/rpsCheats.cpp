164aa1440dc8f6b316a5538ba68b2c17a0949d22
kavisherlock <kavishrmunjal@gmail.com>
2022-12-02 11:22:48 -0600

    Completed day 2

---
+++ day02/part1/rpsCheats.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+const char OPPONENT_ROCK = 'A';
+const char OPPONENT_PAPER = 'B';
+const char OPPONENT_SCISSORS = 'C';
+const char PLAYER_ROCK = 'X';
+const char PLAYER_PAPER = 'Y';
+const char PLAYER_SCISSORS = 'Z';
+const int LOSS_SCORE = 0;
+const int DRAW_SCORE = 3;
+const int WIN_SCORE = 6;
+
+int getRoundScore(char player, char opponent) {
+  int playerChoiceScore = (int)(player - PLAYER_ROCK) + 1;
+  int roundScore = 0;
+
+  if (player - PLAYER_ROCK == opponent - OPPONENT_ROCK) {
+    roundScore = DRAW_SCORE;
+  } else {
+    if (player == PLAYER_ROCK) {
+      roundScore = opponent == OPPONENT_PAPER ? LOSS_SCORE : WIN_SCORE;
+    }
+    if (player == PLAYER_PAPER) {
+      roundScore = opponent == OPPONENT_SCISSORS ? LOSS_SCORE : WIN_SCORE;
+    }
+    if (player == PLAYER_SCISSORS) {
+      roundScore = opponent == OPPONENT_ROCK ? LOSS_SCORE : WIN_SCORE;
+    }
+  }
+
+  return playerChoiceScore + roundScore;
+}
+
+int main () {
+  ifstream myfile;
+  myfile.open ("../input");
+  vector<string> lines;
+  int totalScore = 0;
+  if (myfile.is_open()) {
+    string lineString;
+    while(getline(myfile, lineString)) {
+      char opponent = lineString[0];
+      char player = lineString[2];
+
+      totalScore += getRoundScore(player, opponent);
+    }
+  }
+  myfile.close();
+
+  cout << totalScore << endl;
+}
---
+++ day02/part2/rpsCheats.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+const char ROCK = 'A';
+const char PAPER = 'B';
+const char SCISSORS = 'C';
+const char LOSE = 'X';
+const char DRAW = 'Y';
+const char WIN = 'Z';
+const int LOSS_SCORE = 0;
+const int DRAW_SCORE = 3;
+const int WIN_SCORE = 6;
+
+char getPlayerChoice(char opponent, char intendedResult) {
+  if (intendedResult == DRAW) {
+    return opponent;
+  }
+  if (intendedResult == WIN) {
+    return opponent + 1 > SCISSORS ? ROCK : opponent + 1;
+  }
+  return opponent - 1 < ROCK ? SCISSORS : opponent - 1;
+}
+
+int getRoundScore(char opponent, char intendedResult) {
+  char player = getPlayerChoice(opponent, intendedResult);
+  int playerChoiceScore = (int)(player - ROCK) + 1;
+  int roundScore = intendedResult == WIN ? 6 : (intendedResult == DRAW ? 3 : 0);
+
+  return playerChoiceScore + roundScore;
+}
+
+int main () {
+  ifstream myfile;
+  myfile.open ("../input");
+  vector<string> lines;
+  int totalScore = 0;
+  if (myfile.is_open()) {
+    string lineString;
+    while(getline(myfile, lineString)) {
+      char opponent = lineString[0];
+      char intendedResult = lineString[2];
+
+      totalScore += getRoundScore(opponent, intendedResult);
+    }
+  }
+  myfile.close();
+
+  cout << totalScore << endl;
+}
