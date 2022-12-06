c42a28b7e8cf34c94571f99f33b2681b32a9376d
kavisherlock <kavishrmunjal@gmail.com>
2022-12-05 18:34:56 -0600

    Completed day 5

---
+++ day05/notes.md
@@ -0,0 +1,3 @@
+First notes file for this year. The problems so far have been pretty simple. Which is nice since I'm getting back into C++
+
+This one was also fairly straightforward, with just some string manupulation required. For part one, I was able to use the handy push_back and pop_back functions that come with a c++ standard string that make them behave essentially like a stack, which was exactly what I needed for this problem. And for part two, I had to replace it with doing a substring since we wanted to keep the order.
---
+++ day05/part1/crateStacks.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+vector<string> split(string s, string delimiter) {
+  vector<string> results;
+  size_t pos = 0;
+  string token;
+  while ((pos = s.find(delimiter)) != string::npos) {
+      token = s.substr(0, pos);
+      results.push_back(token);
+      s.erase(0, pos + delimiter.length());
+  }
+  results.push_back(s);
+  return results;
+}
+
+int main () {
+  ifstream myfile;
+  myfile.open ("../input");
+  vector<string> lines;
+  if (myfile.is_open()) {
+    string lineString;
+    while(getline(myfile, lineString)) {
+      lines.push_back(lineString);
+    }
+  }
+  myfile.close();
+
+  int stepIndex = -1;
+  bool stepsFound = false;
+  while (!stepsFound) {
+    stepsFound = lines[++stepIndex][0] == 'm';
+  }
+  
+  int nStacks = (lines[stepIndex - 2].size() + 1) / 4; // each stack required four chars in the input
+  string stacks[nStacks];
+  int curStackInd = stepIndex - 3;
+  while (curStackInd >= 0) {
+    for (int i = 0; i < nStacks; i += 1) {
+      char crate = lines[curStackInd][(i * 4) + 1];
+      if (crate >= 'A' && crate <= 'Z')
+        stacks[i].push_back(crate);
+    }
+    curStackInd -= 1;
+  }
+  
+  for (int i = stepIndex; i < lines.size(); i += 1) {
+    string step = lines[i];
+    vector<string> stepStuff = split(step, " ");
+    int nStacksToMove = stoi(stepStuff[1]);
+    int fromStack = stoi(stepStuff[3]) - 1;
+    int toStack = stoi(stepStuff[5]) - 1;
+
+    for (int i = 0; i < nStacksToMove; i += 1) {
+      stacks[toStack].push_back(stacks[fromStack].back());
+      stacks[fromStack].pop_back();
+    }
+  }
+
+  for (string stack : stacks) {
+    cout << stack[stack.size() - 1];
+  }
+  cout << endl;
+}
